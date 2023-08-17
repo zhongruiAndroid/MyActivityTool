c8cdbacc9ae8f42366e2c54c42066ea22424db43
zhongrui <2380253499@qq.com>
2023-08-17 20:56:51 +0800

    update file

--- app/src/main/java/com/test/activitytool/BlankFragment.java
+++ app/src/main/java/com/test/activitytool/BlankFragment.java
@@ -1,9 +1,6 @@
 package com.test.activitytool;
 
-import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.LayoutInflater;
--- app/src/main/java/com/test/activitytool/MainActivity.java
+++ app/src/main/java/com/test/activitytool/MainActivity.java
@@ -2,7 +2,6 @@ package com.test.activitytool;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
